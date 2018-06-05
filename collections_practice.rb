412b887023b26e9d7fdf7db7699070ed241e494b
Hillevi Eklöw <hillevieklow@gmail.com>
2018-06-05 16:38:36 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -17,3 +17,12 @@ end
 def reverse_array(array)
   array.reverse
 end
+
+def kesha_maker(array)
+  kesha_array = []
+  array.each do |letter|
+    letter[2] = "$"
+    kesha_array << letter
+  end
+  kesha_array
+end
